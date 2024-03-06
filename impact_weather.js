785ce781959abb34c84bee1a79b6ca7cb8d8853a
efgronski <efgronski@gmail.com>
2024-03-05 19:44:35 -0800

    attempting to draw lines on first graph, needs fixed

--- impact_weather.js
+++ impact_weather.js
@@ -1,11 +1,11 @@
 // impactful weather dashboard code
 
-let selectedList = []
+var selectedList = []
 
 var checkboxChange = function(value){
 
     if(selectedList.includes(value) == true){
-        let filteredList = selectedList.filter(function (city) {
+        var filteredList = selectedList.filter(function (city) {
             return city !== value;
         });
         selectedList = filteredList
@@ -16,68 +16,86 @@ var checkboxChange = function(value){
     updateChart(selectedList)
 }
 
-// load data and compute graphs
+// global variables
 
 var citydata
+var precipG
+var highG
+var lowG
+var colorScale
+var lineInterpolate
+
+// load data and compute graphs
 
 d3.csv('impact_weather.csv').then(function(dataset) {
 
+    // variables
+    
     citydata = dataset
 
-    var dateDomain = [new Date(2014, 7, 1), new Date(2015, 6, 30)]
+    var dateDomain = [new Date(2014, 7, 1), new Date(2015, 6, 30)];
 
-    var precip = citydata.map(x => x['actual_precipitation'])
-    var precipDomain = [Math.min(...precip), Math.max(...precip)]
+    var precip = citydata.map(x => x['actual_precipitation']);
+    var precipDomain = [Math.min(...precip), Math.max(...precip)];
     
-    var tempHigh = citydata.map(x => x['record_max_temp'])
-    var tempLow = citydata.map(x => x['record_min_temp'])
-    var tempHighDomain = [Math.min(...tempHigh), Math.max(...tempHigh)]
-    var tempLowDomain = [Math.min(...tempLow), Math.max(...tempLow)]
+    var tempHigh = citydata.map(x => x['record_max_temp']);
+    var tempLow = citydata.map(x => x['record_min_temp']);
+    var tempHighDomain = [Math.min(...tempHigh), Math.max(...tempHigh)];
+    var tempLowDomain = [Math.min(...tempLow), Math.max(...tempLow)];
 
+    // scales for top graph
 
-    let xScaleFirst = d3.scaleTime()
+    var xScaleFirst = d3.scaleTime()
         .domain(dateDomain)
         .range([0, 1130]);
 
-    let yScaleFirst = d3.scaleLinear()
+    var yScaleFirst = d3.scaleLinear()
         .domain(precipDomain)
         .range([300, 0]);
 
-    let xGridFirst = d3.axisTop(xScaleFirst)
+    // create grids for top graph
+
+    var xGridFirst = d3.axisTop(xScaleFirst)
         .tickSize(-300, 0, 0)
-        .tickFormat('')
+        .tickFormat('');
 
-    let yGridFirst = d3.axisLeft(yScaleFirst)
+    var yGridFirst = d3.axisLeft(yScaleFirst)
         .tickSize(-1130, 0, 0)
-        .tickFormat('')
+        .tickFormat('');
 
-    let xScaleSecond = d3.scaleTime()
+    // create scales for bottom grids
+
+    var xScaleSecond = d3.scaleTime()
         .domain(dateDomain)
         .range([0, 660]);
 
-    let yScaleSecondHigh = d3.scaleLinear()
+    var yScaleSecondHigh = d3.scaleLinear()
         .domain(tempHighDomain)
         .range([280, 0]);
 
-    let yScaleSecondLow = d3.scaleLinear()
+    var yScaleSecondLow = d3.scaleLinear()
         .domain(tempLowDomain)
         .range([280, 0]);
 
-    let xGridSecond = d3.axisTop(xScaleSecond)
+    // create grids for bottom two graphs
+
+    var xGridSecond = d3.axisTop(xScaleSecond)
         .tickSize(-280, 0, 0)
-        .tickFormat('')
+        .tickFormat('');
 
-    let yGridSecondHigh = d3.axisLeft(yScaleSecondHigh)
+    var yGridSecondHigh = d3.axisLeft(yScaleSecondHigh)
         .tickSize(-660, 0, 0)
-        .tickFormat('')
+        .tickFormat('');
 
-    let yGridSecondLow = d3.axisLeft(yScaleSecondLow)
+    var yGridSecondLow = d3.axisLeft(yScaleSecondLow)
         .tickSize(-660, 0, 0)
-        .tickFormat('')
+        .tickFormat('');
 
-    let precipG = d3.select('.precipitation')
-    let highG = d3.select('.record-high')
-    let lowG = d3.select('.record-low')
+    precipG = d3.select('.precipitation');
+    highG = d3.select('.record-high');
+    lowG = d3.select('.record-low');
+
+    // append grids for graphs
 
     precipG.append('g')
         .attr('class', 'x grid')
@@ -91,63 +109,95 @@ d3.csv('impact_weather.csv').then(function(dataset) {
 
     highG.append('g')
         .attr('class', 'x grid')
-        .attr('transform','translate(' + [25, 0] + ')')
+        .attr('transform','translate(' + [25, 10] + ')')
         .call(xGridSecond);
 
     highG.append('g')
         .attr('class', 'y grid')
-        .attr('transform','translate(' + [25, 0] + ')')
+        .attr('transform','translate(' + [25, 10] + ')')
         .call(yGridSecondHigh);
 
     lowG.append('g')
         .attr('class', 'x grid')
-        .attr('transform','translate(' + [25, 0] + ')')
+        .attr('transform','translate(' + [25, 10] + ')')
         .call(xGridSecond);
 
     lowG.append('g')
         .attr('class', 'y grid')
-        .attr('transform','translate(' + [25, 0] + ')')
+        .attr('transform','translate(' + [25, 10] + ')')
         .call(yGridSecondLow);
 
-    let xAxisFirst = d3.axisBottom(xScaleFirst);
+    // axes for precip graph
+
+    var xAxisFirst = d3.axisBottom(xScaleFirst);
         precipG.append('g')
             .attr('class', 'x axis')
             .attr('transform','translate(' + [25, 300] + ')')
             .call(xAxisFirst);
 
-    let yAxisFirst = d3.axisLeft(yScaleFirst);
+    var yAxisFirst = d3.axisLeft(yScaleFirst);
         precipG.append('g')
             .attr('class', 'y axis')
             .attr('transform','translate(' + [25, 0] + ')')
             .call(yAxisFirst);
 
-    d3.selectAll('rect').attr('transform', function(f){
-        return 'translate('+[25,0]+')'
-    })
+    // reposition rectangles to fit in svgs
 
-    let xAxisSecondHigh = d3.axisBottom(xScaleSecond);
+    d3.selectAll('.precipitation rect').attr('transform', function(f){
+        return 'translate('+[25,0]+')'
+    });
+
+    d3.selectAll('.record-low rect').attr('transform', function(f){
+        return 'translate('+[25,10]+')'
+    });
+
+    d3.selectAll('.record-high rect').attr('transform', function(f){
+        return 'translate('+[25,10]+')'
+    });
+
+    // d3.selectAll('.fast-facts').attr('transform', function(f){
+    //     return 'translate('+[10,10]+')'
+    // });
+
+    // axes for bottom two graphs
+
+    var xAxisSecondHigh = d3.axisBottom(xScaleSecond);
         highG.append('g')
             .attr('class', 'x axis')
-            .attr('transform','translate(' + [25, 280] + ')')
+            .attr('transform','translate(' + [25, 290] + ')')
             .call(xAxisSecondHigh);
 
-    let yAxisSecondHigh = d3.axisLeft(yScaleSecondHigh);
+    var yAxisSecondHigh = d3.axisLeft(yScaleSecondHigh);
         highG.append('g')
             .attr('class', 'y axis')
-            .attr('transform','translate(' + [25, -20] + ')')
+            .attr('transform','translate(' + [25, 10] + ')')
             .call(yAxisSecondHigh);
 
-    let xAxisSecondLow = d3.axisBottom(xScaleSecond);
+    var xAxisSecondLow = d3.axisBottom(xScaleSecond);
         lowG.append('g')
             .attr('class', 'x axis')
-            .attr('transform','translate(' + [25, 280] + ')')
+            .attr('transform','translate(' + [25, 290] + ')')
             .call(xAxisSecondLow);
 
-    let yAxisSecondLow = d3.axisLeft(yScaleSecondLow);
+    var yAxisSecondLow = d3.axisLeft(yScaleSecondLow);
         lowG.append('g')
             .attr('class', 'y axis')
-            .attr('transform','translate(' + [25, -20] + ')')
+            .attr('transform','translate(' + [25, 10] + ')')
             .call(yAxisSecondLow);
+
+    // others
+
+    lineInterpolate = d3.line()
+        .x(function(d) { return xScale(d.date); })
+        .y(function(d) { return yScale(d.price); });
+
+    var defaultCityNest = d3.nest()
+        .key(function(c) {
+            return c.city_code;
+        })
+        .entries(citydata);
+
+    colorScale = d3.scaleOrdinal(defaultCityNest.map((x) => x['key']), d3.schemeCategory10);
 
 });
 
@@ -156,7 +206,7 @@ function updateChart(selectedList){
     // filter the data based on the selections made by user
     var filteredDataset
 
-    var map1 = selectedList.map((city) => citydata.filter((row) => row.city_code == city))
+    var map1 = selectedList.map((city) => citydata.filter((row) => row.city_code == city));
     
     if (map1.length == 0){
         filteredDataset = []
@@ -174,14 +224,20 @@ function updateChart(selectedList){
         filteredDataset = map1[0].concat(map1[1], map1[2], map1[3], map1[4], map1[5])
     } else {
         filteredDataset = citydata
-    }
+    };
 
     // create nest of each city in dataset
-    // var cityNest = d3.nest()
-    //     .key(function(c) {
-    //         return c.city_code;
-    //     })
-    //     .entries(filteredDataset)
+    var cityNest = d3.nest()
+        .key(function(c) {
+            return c.city_code;
+        })
+        .entries(filteredDataset);
 
-    // console.log(cityNest)
+    precipG.selectAll('.precipitation')
+        .data(cityNest)
+        .enter()
+        .append('path')
+            .attr('class', 'line-plot')
+            .style('stroke', function(d) {return colorScale(cityNest[0].citycode)  })
+            .attr('d', lineInterpolate);
 }
